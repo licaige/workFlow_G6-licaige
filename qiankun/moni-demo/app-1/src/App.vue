a9f1df753a5fd5048bb494a0bf463e0eb08764ab
“李琦” <qi.li@hongpucorp.com>
2024-03-06 15:11:44 +0800

    feat:demo

---
+++ qiankun/moni-demo/app-1/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+     <div class="container">
+       欢迎使用vue3+vite
+     </div>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+.container{
+  color: red;
+}
+</style>
