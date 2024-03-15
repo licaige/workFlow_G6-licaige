f22204f33ce8c094c43f8b262c5afd19ccb97be8
“李琦” <qi.li@hongpucorp.com>
2024-03-15 16:25:04 +0800

    feat: 备注信息

--- qiankun/video-code/main/micro/sandbox/snapShotSandbox.js
+++ qiankun/video-code/main/micro/sandbox/snapShotSandbox.js
@@ -4,14 +4,12 @@ export class SnapShotSandbox {
   constructor() {
     // 1. 代理对象
     this.proxy = window
-
     this.active()
   }
   // 沙箱激活
   active() {
     // 创建一个沙箱快照
     this.snapshot = new Map()
-
     // 遍历全局环境
     for(const key in window) {
       this.snapshot[key] = window[key]
